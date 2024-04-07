cd2554dcc9ddfb496435ec70f4838be2c6b9a14e
samuelds <samuelds.pro@gmail.com>
2024-04-07 21:55:05 +0200

    Setup project

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Sass support
+    sassOptions: {
+        includePaths: ['./src/styles'],
+    }
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import "@/styles/customBootstrap.scss";
+
+export default function Home() {
+    return (
+        <main className='container'>
+            <h1 className='text-center'>NextJS Application ...</h1>
+            <button className='btn btn-primary m-2'>Button</button>
+            <button className='btn btn-secondary m-2'>Button</button>
+            <button className='btn btn-danger m-2'>Button</button>
+        </main>
+    )
+}
